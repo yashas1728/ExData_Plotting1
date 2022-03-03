486af3a1ff4dbaa4e00502adbcfda11d8a94d98f
yashas1728 <100117083+yashas1728@users.noreply.github.com>
2022-03-03 12:55:56 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,7 +1,5 @@
-data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data <- read.table("/Users/yashasbharadwaj/Desktop/coursera/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 newData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-
 datetime <- strptime(paste(newData$Date, newData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(newData$Global_active_power)
 globalReactivePower <- as.numeric(newData$Global_reactive_power)
@@ -9,7 +7,6 @@ voltage <- as.numeric(newData$Voltage)
 subMetering1 <- as.numeric(newData$Sub_metering_1)
 subMetering2 <- as.numeric(newData$Sub_metering_2)
 subMetering3 <- as.numeric(newData$Sub_metering_3)
-
 
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) #attributes mfrow is row 2 col 2 here
