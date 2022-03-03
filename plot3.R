7a96a7aa0ada28313f8358f933710f0fabcc3dcd
yashas1728 <100117083+yashas1728@users.noreply.github.com>
2022-03-03 12:55:23 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,7 +1,6 @@
-dataFile <- "household_power_consumption.txt"
+dataFile <- "/Users/yashasbharadwaj/Desktop/coursera/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 newData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
 
 datetime <- strptime(paste(newData$Date, newData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(newData$Global_active_power)
